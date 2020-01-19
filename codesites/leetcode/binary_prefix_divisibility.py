6e2925adb3e5ee5d7469cb9dc51f270f3def8b12
Rahul Bali <rahulrdb18@gmail.com>
2020-01-19 17:23:46 +0530

    adjustment of folder structure

---
+++ codesites/leetcode/binary_prefix_divisibility.py
@@ -0,0 +1,8 @@
+
+class Solution:
+	def prefixesDivBy5(self, A: List[int]) -> List[bool]:
+		qwerty = [7, 8, 9, 4, 6]
+		print(qwerty[0])
+
+
+Solution.prefixesDivBy5([4, 6, 8])
--- leetcode/binary_prefix_divisibility.py
+++
@@ -1,8 +0,0 @@
-
-class Solution:
-	def prefixesDivBy5(self, A: List[int]) -> List[bool]:
-		qwerty = [7, 8, 9, 4, 6]
-		print(qwerty[0])
-
-
-Solution.prefixesDivBy5([4, 6, 8])
