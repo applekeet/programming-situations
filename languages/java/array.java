afaf35b7a3bcce1ff4ae36f9d3d575bf29ca1624
Rahul Bali <rahulrdb18@gmail.com>
2020-01-27 16:24:25 +0530

    cpp java changes

---
+++ languages/java/array.java
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/java-negative-subarray/problem
+
+
+class Array1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+    }
+}
+
+
